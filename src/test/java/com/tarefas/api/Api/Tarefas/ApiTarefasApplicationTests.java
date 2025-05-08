a064923f12bfecd421f178a3b596ff0eaaf3052d
Tiago Souza <tiagoadesouza21@gmail.com>
2025-05-08 08:25:36 -0300

    Adicionando primeiros arquivos

---
+++ src/test/java/com/tarefas/api/Api/Tarefas/ApiTarefasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tarefas.api.Api.Tarefas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiTarefasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
